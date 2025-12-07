bc2ae90810ec660f170306929e0de941a43c804b
samue <xsmatrala@stuba.sk>
2025-12-07 11:23:11 +0100

    transpose uprava

--- traspose.py
+++ traspose.py
@@ -3,23 +3,17 @@ import csv
 with open('static/pollenAverageLoads.csv', newline="") as csvfile:
     reader = csv.reader(csvfile)
     data = list(reader)
-print(data)
 
-y = 4
-pocetRokov = 366
+y = len(data[0]) #dlzka riadku
+pocetRokov = len(data) #dlzka stlpca
 
-
-header = [data[0][0], data[1][0], data[2][0], data[3][0]]
-row_names = data[0][1::]
-print(row_names)
 
 with open("static/pollenAverageLoads.csv1", "w", newline="",
           encoding="utf-8") as f:
     writer = csv.writer(f)
-    writer.writerow(header)
 
-    for i in range(1, pocetRokov-1):
-        row = [row_names[i]]
-        for rok in range(1, y):
+    for i in range(y):
+        row = []
+        for rok in range(pocetRokov):
             row.append(data[rok][i])
         writer.writerow(row)
